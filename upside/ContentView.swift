9127bea5ec1ec4cb1876289028c16994254c0bdf
Nik <nik.dmello@gmail.com>
2026-02-03 18:22:24 -0600

    feat: landing screen

---
+++ upside/ContentView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        WelcomeView(onContinue: {
+            // TODO: Navigate to next screen
+        })
+    }
+}
+
+#Preview {
+    ContentView()
+}
